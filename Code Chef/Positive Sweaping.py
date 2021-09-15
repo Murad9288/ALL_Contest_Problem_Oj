6efa444f75b0eb2fa6cdd9ad089fd94471aa18a6
MD Murad Hossain <81400169+Murad9288@users.noreply.github.com>
2021-09-16 00:21:09 +0600

    Update Positive Sweaping.py

--- Code Chef/Positive Sweaping.py
+++ Code Chef/Positive Sweaping.py
@@ -2,10 +2,29 @@
 # Problem name: Positive Sweping.
 try:
     for _ in range(int(input())):
-        n = int(input())
-        li = list(map(int,input().split()))
-        result = max(li)
-        res = (((10**5)-result)+((10**5)-result+1))
-        print(res)
+        n,s=map(int,input().split())
+        l=list(map(int,input().split()))
+        e=l.copy()
+        if any(l)>0:
+            q=0
+            for i in range(s):
+                q+=1
+                for j in range(len(l)):
+                    if l[j]>0:
+                        if j+1<n:
+                            e[j+1]+=1
+                        else:
+                            e[0]+=1
+                        if j-1>=0:
+                            e[j-1]+=1
+                        else:
+                            e[n-1]+=1
+                l=e.copy()
+                if all(e)>0:
+                    break
+            ans=sum(e)+n*(s-q)*2
+            print(ans)
+        else:
+            print(0)
 except:
     pass
