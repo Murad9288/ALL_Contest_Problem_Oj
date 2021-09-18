c0dd2162ad17b76198bdd4fe4d356328f534cfa4
MD Murad Hossain <81400169+Murad9288@users.noreply.github.com>
2021-09-18 13:23:19 +0600

    Delete Vaiyer Problem directory

--- Vaiyer Problem/File Cheak korar program.py
+++
@@ -1,4 +0,0 @@
-import os
-print(os.path.getsize("G://my work"))
-print("Majhe Majhe tobo dekha pai, Chirodin keno paina...")
-
--- Vaiyer Problem/Problem - 3(Tanish ball).py
+++
@@ -1,5 +0,0 @@
-r = float(input())
-pi = 3.141
-s = pi*r**3
-print("%.3f" %s)
- 
