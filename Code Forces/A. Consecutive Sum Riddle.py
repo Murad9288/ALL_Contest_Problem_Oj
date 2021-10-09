2a90492a7c73f56f1f85697a52b82e31a51f8f80
MD Murad Hossain <81400169+Murad9288@users.noreply.github.com>
2021-10-10 01:43:56 +0600

    Update A. Consecutive Sum Riddle.py

--- Code Forces/A. Consecutive Sum Riddle.py
+++ Code Forces/A. Consecutive Sum Riddle.py
@@ -1,1 +1,7 @@
-for _ in range(int(input())):  n = int(input())  if n == 1:    print(0,1)  else:    print("-"+str(n-1),n)
+for _ in range(int(input())):
+  n = int(input())
+  if n == 1:
+    print(0,1)
+  else:
+    print("-"+str(n-1),n)
+    
