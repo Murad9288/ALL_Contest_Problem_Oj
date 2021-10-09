38e2c04a367268c393195430420a477bb5ac9cbd
MD Murad Hossain <81400169+Murad9288@users.noreply.github.com>
2021-10-10 01:46:20 +0600

    Update G - Years.py

--- Code Forces/G - Years.py
+++ Code Forces/G - Years.py
@@ -1,1 +1,16 @@
-x = []for _ in range(int(input())):    a,b = map(int,input().split())    x.append([a,1])    x.append([b,-1])x.sort()y = []max = -1cur = 0for i in range(len(x)):    cur += x[i][1]    if(cur>max):        max = cur        y = x[i][0]print(y,max)
+x = []
+for _ in range(int(input())):
+    a,b = map(int,input().split())
+    x.append([a,1])
+    x.append([b,-1])
+x.sort()
+y = []
+max = -1
+cur = 0
+for i in range(len(x)):
+    cur += x[i][1]
+    if(cur>max):
+        max = cur
+        y = x[i][0]
+
+print(y,max)
