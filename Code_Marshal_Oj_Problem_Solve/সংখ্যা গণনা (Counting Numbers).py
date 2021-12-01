2b7517e1e8c2caea5a6b15ddadfc1bf37a005727
MD Murad Hossain <81400169+Murad9288@users.noreply.github.com>
2021-12-01 20:59:44 +0600

    Add files via upload

---
+++ Code_Marshal_Oj_Problem_Solve/সংখ্যা গণনা (Counting Numbers).py
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int testcase;
+    cin >> testcase;
+
+    for(int i = 1; i <= testcase; i++)
+    {
+        int n, c[101] = {0}, tmp;
+        cin >> n;
+        for(int j = 0; j < n; j++)
+        {
+          cin>>tmp;
+          c[tmp]++;
+        }
+
+        int h = 0,m;
+        for(int j = 0; j <= 100; j++)
+        {
+            if(c[j] >= h)
+            {
+                h = c[j];
+                m = j;
+            }
+        }
+        cout<<"Case "<< i <<": "<< m <<" "<< h <<endl;
+    }
+
+}
