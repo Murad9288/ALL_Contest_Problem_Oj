2d3a146f84814382a6ec2e28959f0e61ebb4f254
MD Murad Hossain <81400169+Murad9288@users.noreply.github.com>
2021-12-01 15:01:18 +0600

    Add files via upload

---
+++ Code_Marshal_Oj_Problem_Solve/ত্রিভুজের ক্ষেত্রফল (Area of Triangle).py
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include<math.h>
+
+int main ()
+{
+    int i, j, n;
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++)
+    {
+        double a, b, c;
+        double s = 0, area = 0;
+        scanf("%lf %lf %lf", &a, &b, &c);
+
+        s = (a+b+c)/2;
+
+        area = sqrt(s * (s-a) * (s-b) * (s-c));
+
+        printf("Case %d: %.10lf\n", i, area);
+    }
+
+    return 0;
+}
