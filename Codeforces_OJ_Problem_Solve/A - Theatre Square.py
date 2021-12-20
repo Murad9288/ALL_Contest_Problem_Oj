49c097c51faf3ba48fe140b8038c108bb7e5bb23
MD Murad Hossain <81400169+Murad9288@users.noreply.github.com>
2021-12-21 00:53:19 +0600

    Rename A. Theatre Square.py to A - Theatre Square.py

---
+++ Codeforces_OJ_Problem_Solve/A - Theatre Square.py
@@ -0,0 +1,3 @@
+import math
+n,m,a = map(int,input().split())
+print(math.ceil(n/a) * math.ceil(m/a))
--- Codeforces_OJ_Problem_Solve/A. Theatre Square.py
+++
@@ -1,1 +0,0 @@
-import mathn,m,a = map(int,input().split())print(math.ceil(n/a) * math.ceil(m/a))
