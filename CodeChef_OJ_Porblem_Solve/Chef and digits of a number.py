1add76ded9ebf96f70c9339e5514a10a38712d83
MD Murad Hossain <81400169+Murad9288@users.noreply.github.com>
2022-01-31 00:37:31 +0600

    Add files via upload

---
+++ CodeChef_OJ_Porblem_Solve/Chef and digits of a number.py
@@ -0,0 +1,9 @@
+try:
+    for _ in range(int(input())):
+        n = input()
+        if n.count("1") == 1 or n.count("0") == 1:
+            print("Yes")
+        else:
+            print("No")
+except:
+    pass
