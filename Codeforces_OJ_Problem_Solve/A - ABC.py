05eb43a9c1eca6556be9839d9470ad068920656c
MD Murad Hossain <81400169+Murad9288@users.noreply.github.com>
2022-01-31 00:41:12 +0600

    Add files via upload

---
+++ Codeforces_OJ_Problem_Solve/A - ABC.py
@@ -0,0 +1,7 @@
+for _ in range(int(input())):
+    n = int(input())
+    a = str(input())
+    if (a == '1' or a == '0' or a == '10' or a == '01'):
+        print('YES')
+    else:
+        print('NO')
